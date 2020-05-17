09cd07ecaa4d0825fb64300f728cfd47ea311021
Conor Byrne <con0r.b@protonmail.com>
2020-05-17 18:08:11 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# classroom-cli
+classroom-cli is a python application designed for [Google Classroom](https://classroom.google.com)
+
+## Setting up
+1. Make sure you have the latest Python Version installed
+2. Download this repository as a [zip](http://github.com/cbyrne/classroom-cli/archive/master.zip) or clone it
+    - ``git clone git@github.com:cbyrne/classroom-cli.git``
+3. Install the dependencies required:
+    - ``pip3 install -r requirements.txt``
+4. Download the credentials.json when you click **``Enable the Classroom API``** from [here](https://developers.google.com/classroom/quickstart/python#step_1_turn_on_the) and put it in the root directory of this project.
+    - Your folder structure should look like this
+
+      ![image](https://cdn.discordapp.com/attachments/681075622013042743/711624469323579402/unknown.png)
+5. Run classroom-cli!
+    - ``python3 index.py``
---
+++ index.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = [
+    'https://www.googleapis.com/auth/classroom.courses.readonly',
+    'https://www.googleapis.com/auth/classroom.rosters',
+    'https://www.googleapis.com/auth/classroom.rosters.readonly',
+    'https://www.googleapis.com/auth/classroom.profile.emails',
+    'https://www.googleapis.com/auth/classroom.profile.photos',
+]
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+def initialize():
+    creds = None
+
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    global service
+    service = build('classroom', 'v1', credentials=creds)
+
+def menu():
+    print(color.BOLD + "Type a command or use 'h' or 'help' for help" + color.END)
+
+    help(input(color.BLUE + "> " + color.END))
+
+def help(command):
+    if(command == "h" or command == "help"):
+        print(color.BLUE + "classroom-cli v1.0 - help menu\n" + color.END + "h" + color.END + " | displays this menu")
+    
+    menu()
+
+
+def getCourses():
+    results = service.courses().list(pageSize=100).execute()
+    courses = results.get('courses', [])
+
+    if not courses:
+        print('No courses found.')
+    else:
+        print('Courses:')
+        for course in courses:
+            print(course['name'])
+
+if __name__ == '__main__':
+    initialize()
+
+    student = service.userProfiles().get(userId="me").execute()
+    name = student.get("name").get("fullName")
+
+    print(color.GREEN  + color.BOLD + "You are logged in as " + name + color.END)
+
+    menu()
