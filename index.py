d62f7ca154f23aac7e5dccb07c102142548a4fca
Conor <31479541+cbyrne@users.noreply.github.com>
2020-09-08 18:54:31 +0100

    Move type a command message to __main__

--- index.py
+++ index.py
@@ -1,17 +1,19 @@
 from utils import color, gclassroom, logger
 
-Classroom = gclassroom.Classroom() 
-ClassroomHelper = gclassroom.ClassroomHelper(classroom=Classroom) 
+Classroom = gclassroom.Classroom()
+ClassroomHelper = gclassroom.ClassroomHelper(classroom=Classroom)
 Color = color.Color()
 Logger = logger.Logger()
+
 
 def help():
     Logger.notice("classroom-cli v0.0.1:")
     print("h (help) | Displays this menu\nlc (listcourses) | Lists courses that you are enrolled in\nla (listassignments) | Lists assignments that are due to be turned in\nexit (stop, x) | Closes the application")
 
+
 def menu():
-    Logger.info("Type a command or use 'h' or 'help' for help")
     parseCommand(input(Color.BLUE + "> " + Color.END))
+
 
 def parseCommand(command):
     if command in ("h", "help"):
@@ -24,8 +26,9 @@ def parseCommand(command):
         exit(0)
     else:
         Logger.error("Unknown command!")
-        
+
     menu()
+
 
 if __name__ == '__main__':
     Classroom.initialize()
@@ -35,5 +38,6 @@ if __name__ == '__main__':
     name = student.get("name").get("fullName")
 
     Logger.success(Color.BOLD + "You are logged in as " + name)
+    Logger.info("Type a command or use 'h' or 'help' for help")
 
     menu()
